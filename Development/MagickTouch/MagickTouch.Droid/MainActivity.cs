c7b88f0aba42a58ec899896d6b0a840d10e18eec
ed sm <ed sm>
2017-01-23 18:31:07 -0300

    first commit

---
+++ Development/MagickTouch/MagickTouch.Droid/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using static Android.Resource;
+
+namespace MagickTouch.Droid
+{
+    [Activity(Label = "MagickTouch.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        ImageView iw;
+        Button button;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            button = FindViewById<Button>(Resource.Id.myButton);
+
+            iw = FindViewById<ImageView>(Resource.Id.imageView1);
+
+            iw.Touch += TouchMeImageViewOnTouch;
+
+            iw.SetColorFilter(Android.Graphics.Color.Argb(255, 128, 128, 128));
+
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+
+        }
+
+        private void TouchMeImageViewOnTouch(object sender, View.TouchEventArgs touchEventArgs)
+        {
+            switch (touchEventArgs.Event.Action)
+            {
+                case MotionEventActions.Down:
+                    button.Text = "0";
+                    iw.SetColorFilter(Android.Graphics.Color.Argb(255, 0, 128, 0));
+                    break;
+                case MotionEventActions.Pointer1Down:
+                    button.Text = "1";
+                    
+                break;
+                case MotionEventActions.Pointer2Down:
+                    button.Text = "2";
+                    iw.SetColorFilter(Android.Graphics.Color.Argb(255, 128, 0, 0));
+                    break;
+                case MotionEventActions.Pointer3Down:
+                    iw.SetColorFilter(Android.Graphics.Color.Argb(255, 128, 0, 0));
+                    button.Text = "3";
+                break;
+                default:
+                    // button.Text = "";
+                    // iw.SetColorFilter(Android.Graphics.Color.Argb(255, 128, 128, 128));
+                    break;
+            }
+        }
+
+    }
+
+    }
+
+
---
+++ Development/MagickTouch/MagickTouch.Droid/obj/Debug/android/src/md5884776f7e2d6ecef3ff360a3c21e9f3e/MainActivity.java
@@ -0,0 +1,46 @@
+package md5884776f7e2d6ecef3ff360a3c21e9f3e;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("MagickTouch.Droid.MainActivity, MagickTouch.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MagickTouch.Droid.MainActivity, MagickTouch.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
